d8dbedd15e8d5c6ac606fa15c2fc51b26b50799e
Summersisle <25886892+Summersisle@users.noreply.github.com>
2021-07-19 01:43:19 -0700

    Belf 1-13 complete

---
+++ Summersisle Zul'Farrak.lua
@@ -0,0 +1,13 @@
+Guidelime.registerGuide([[
+[D *My* own guide for Guidelime \\ http://www.twitch.tv/mytwitch]
+[GA Horde]
+[NZulFarrak]
+[QA3042 Troll Temper]
+[QA2936 The Spider God]
+[QA2865 Scarab Shells]
+[QA2846 Tiara of the Deep]
+[QA2768 Divino-matic Rod]
+[QA3527 The Prophecy of Mosh'aru]
+[QA2770 Gahz'rilla]
+
+]], "Summersisle's Horde Leveling guide")
