1503c06c38f096db2e0a312e33cba069050a0bd1
Summersisle <25886892+Summersisle@users.noreply.github.com>
2021-08-07 17:38:31 -0700

    Update Bloodelf 10-20.lua
    10-20 Ghostlands

--- Bloodelf 10-20.lua
+++ Bloodelf 10-20.lua
@@ -1,3 +1,154 @@
 Guidelime.registerGuide([[
-[N10-20 Bloodelf]
+[N10-20Ghostlands]
+[QA9144 Missing in the Ghostlands]
+[QT9144 Missing in the Ghostlands]
+[QA9147 The Fallen Courier]
+[QC9147 The Fallen Courier]
+[QT9147 The Fallen Courier]
+[QA9148 Delivery to Tranquillien]
+[QT9148 Delivery to Tranquillien]
+[A BloodElf][QA9327 The Forsaken]
+[A BloodElf][QT9327 The Forsaken]
+[A Orc,Tauren,Undead,Troll][QA9329 The Forsaken]
+[A Orc,Tauren,Undead,Troll][QT9329 The Forsaken]
+[QA9758 Return to Arcanist Vandril]
+[QT9758 Return to Arcanist Vandril]
+[OC][S]Set Hearth at Innkeeper Kalarin[G48.78,32.35Ghostlands]
+[QA9138 Suncrown Village]
+[QA9315 Anok'suten]
+[QC9138 Suncrown Village][QC9315 Anok'suten]
+
+[QT9315 Anok'suten][QT9138 Suncrown Village]
+[OC][V]Sell and buy any useful rep rewards from Provisioner Vredigar[G47.69,32.28Ghostlands]
+[QA9145 Help Ranger Valanna!]
+
+[QT9145 Help Ranger Valanna!]
+[QA9143 Dealing with Zeb'Sora][QA9157 Forgotten Rituals]
+[QA9274 Spirits of the Drowned]
+[QC9143 Dealing with Zeb'Sora][QC9157 Forgotten Rituals][QC9274 Spirits of the Drowned]
+[QT9157 Forgotten Rituals]
+[QA9174 Vanquishing Aquantion]
+[QT9143 Dealing with Zeb'Sora]
+[QA9146 Report to Captain Helios]
+[QC9174 Vanquishing Aquantion]
+[QT9174 Vanquishing Aquantion]
+[QT9146 Report to Captain Helios]
+[QT9274 Spirits of the Drowned]
+[QA9158 Bearers of the Plague]
+[QC9158 Bearers of the Plague]
+[QT9158 Bearers of the Plague]
+[QA9159 Curbing the Plague]
+[QC9159,1 Curbing the Plague]
+[OC][H]Hearth back to Tranquillien
+
+[QA9152 Tomber's Supplies]
+
+
+[QA9150 Salvaging the Past]
+[QA9149 The Plagued Coast]
+[QA9139 Goldenmist Village]
+[QA9192 Trouble at the Underlight Mines]
+[QA9160 Investigate An'daroth]
+[QA9155 Down the Dead Scar]
+[QA9218 Spinal Dust][QA9216 Rotting Hearts]
+[QA9171 Culinary Crunch]
+[A BloodElf][QA9130 Goods from Silvermoon City]
+[A BloodElf][QT9130 Goods from Silvermoon City]
+[A BloodElf][QA9133 Fly to Silvermoon City]
+[OC][F Silvermoon City]
+[A BloodElf][QT9133 Fly to Silvermoon City]
+[A BloodElf][QA9134 Skymistress Gloaming]
+[OC][T]Train Skills
+[OC][V][R]Vendor items and repair
+[A BloodElf][QT9134 Skymistress Gloaming]
+[A BloodElf][QA9135 Return to Quartermaster Lymel]
+[OC][F]Tranquillien, Ghostlands
+[A BloodElf][QT9135 Return to Quartermaster Lymel]
+
+[QC9150 Salvaging the Past]
+[QC9152 Tomber's Supplies]
+[QC9160 Investigate An'daroth]
+[QC9139 Goldenmist Village]
+[QC9149 The Plagued Coast]
+[OC][H]Hearth backTranqulien
+[QT9150 Salvaging the Past][QT9152 Tomber's Supplies][QT9160 Investigate An'daroth][QT9139 Goldenmist Village][QT9149 The Plagued Coast]
+
+[QA9163 Into Occupied Territory]
+[QA9140 Windrunner Village]
+[QA9173 Retaking Windrunner Spire]
+
+
+
+Kill everything along the way to accept [QA9207 Underlight Ore Samples]
+[QC9207 Underlight Ore Samples][QC9192 Trouble at the Underlight Mines]
+
+[QC9140 Windrunner Village]
+[QC9163 Into Occupied Territory]
+[QA9175 The Lady's Necklace][QC9173 Retaking Windrunner Spire]
+[QC9159 Curbing the Plague][QC9171 Culinary Crunch]
+[O][QC9216 Rotting Hearts][QC9155 Down the Dead Scar]
+
+[QT9140 Windrunner Village][QT9173 Retaking Windrunner Spire][QT9163 Into Occupied Territory][QT9175 The Lady's Necklace][QA9192 Trouble at the Underlight Mines][QT9155 Down the Dead Scar]
+[O][QA9216 Rotting Hearts]
+[QA9151 The Sanctum of the Sun][QA9156 Wanted: Knucklerot and Luzran]
+
+[QT9207 Underlight Ore Samples][QT9151 The Sanctum of the Sun][QT9172 Report to Magister Kaendris][QA9176 The Twin Ziggurats][QA9220 War on Deatholme]
+
+[QT9166 Deliver the Plans to An'telas][QA9169 Deactivate An'owyn]
+[QT9159 Curbing the Plague]
+
+[QA9275 A Little Dash of Seasoning][QA9276 Attack on Zeb'Tela]
+[QC9276 Attack on Zeb'Tela] [QC9214,1 Shadowpine Weaponry][QC9214,2 Shadowpine Weaponry]
+[QT9276 Attack on Zeb'Tela][QA9277 Assault on Zeb'Nowa]
+[QC9275 A Little Dash of Seasoning]Kill everything while completing
+[QC9277 Assault on Zeb'Nowa][QC9214,3 Shadowpine Weaponry][QC9214,4 Shadowpine Weaponry][QC9275 A Little Dash of Seasoning]
+
+[QC9275 A Little Dash of Seasoning]
+
+
+
+[QC9215 Bring Me Kel'gash's Head!] This is a level 20 elite, clear out all the mobs around him, make sure you have a food buff, potions, etc. You got this!
+
+[QC9169 Deactivate An'owyn]
+[QT9169 Deactivate An'owyn]
+[OC][V][R]Sell and repair
+[QT9277 Assault on Zeb'Nowa][QT9214 Shadowpine Weaponry][QT9215 Bring Me Kel'gash's Head!]
+
+[QA9212 Escape from the Catacombs]Kill all the trolls and light all the mummies on fire on the way to accept this escort quest
+[QC9212 Escape from the Catacombs][QT9212 Escape from the Catacombs]
+[QC9193 Investigate the Amani Catacombs][QC9199 Troll Juju]
+[OC][H] Hearth back to Tranqellien
+[OC][V]Sell and buy any useful rep rewards from Provisioner Vredigar[G47.69,32.28Ghostlands]
+[QT9193 Investigate the Amani Catacombs][QT9199 Troll Juju]
+[QA9281 Clearing the Way]
+[QC9176 The Twin Ziggurats]
+[QC9281 Clearing the Way]
+[QC9218 Spinal Dust][QC9216 Rotting Hearts][QC9220 War on Deatholme]
+
+[QT9281 Clearing the Way]
+[QT9176 The Twin Ziggurats][QT9220 War on Deatholme][QA9170 Dar'Khan's Lieutenants][QA9167 The Traitor's Destruction][QA9877 A Restorative Draught]
+[QT9877 A Restorative Draught][QT9218 Spinal Dust][QT9216 Rotting Hearts][QA9164 Captives at Deatholme]
+[QC9170 Dar'Khan's Lieutenants][QC9167 The Traitor's Destruction][QC9164 Captives at Deatholme][QC9156 Wanted: Knucklerot and Luzran]
+[QT9167 The Traitor's Destruction][QT9170 Dar'Khan's Lieutenants][QT9164 Captives at Deatholme]
+[A BloodElf][QA9328 Hero of the Sin'dorei]
+[QT9156 Wanted: Knucklerot and Luzran]
+[OC][V][R]You should be exalted now, buy your cape and repair
+[OC][F]Silvermoon City
+[OC][T]Visit your class trainer
+[A BloodElf][QT9328 Hero of the Sin'dorei][QA9621 Envoy to the Horde]
+[OC][G49.4,14.78Silvermoon City]Use the translocation device to portal to UC
+[A BloodElf][QT9621 Envoy to the Horde][QA9626 Meeting the Warchief][QT9180 Journey to Undercity]
+[A BloodElf][QA9189 Delivery to the Sepulcher][QA9425 Report to Tarren Mill]
+[OC][O][G60.62,58.7Tirisfal Glades]Run to the Zepplin to Orgrimmar
+[OC][O]Buy a port to Shattrah and set your [S] to the Scyer Inn
+[OC][O]Take the portal back to Orgrimmar
+[OC][P]Get the Orgrimmar Fligh Point
+[QA235 The Ashenvale Hunt]
+[A BloodElf][QT9621 Envoy to the Horde][QA9627 Allegiance to the Horde][QA9428 Report to Splintertree Post]
+[OC][H] Hearth back to Shattrah
+[A BloodElf][OC]Take the portal to Silvermoon
+[A BloodElf][QT9627 Allegiance to the Horde]
+[A BloodElf][OC][G49.4,14.78Silvermoon City]Use the translocation device to portal to UC
+[OC][A Orc,Tauren,Undead,Troll]Take the portal to Undercity
+
 ]], "Summersisle's Horde Leveling guide")
