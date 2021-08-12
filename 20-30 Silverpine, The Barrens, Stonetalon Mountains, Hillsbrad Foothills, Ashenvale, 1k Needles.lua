f311398132b4fd3d132b02c2eed829061fbca45d
Summersisle <25886892+Summersisle@users.noreply.github.com>
2021-08-12 00:12:25 -0700

    20-30 guide

---
+++ 20-30 Silverpine, The Barrens, Stonetalon Mountains, Hillsbrad Foothills, Ashenvale, 1k Needles.lua
@@ -0,0 +1,172 @@
+Guidelime.registerGuide([[
+[N20-30Silverpine, The Barrens, Stonetalon Mountains, Hillsbrad Foothills, Ashenvale, 1k Needles]
+[A BloodElf][QA9425 Report to Tarren Mill][QA9428 Report to Splintertree Post][QA9189 Delivery to the Sepulcher]
+[QA235 The Ashenvale Hunt]
+[OC][F Sepulcher][G45.63,42.48Silverpine Forest][P Sepulcher]
+[A BloodElf][QT9189 Delivery to the Sepulcher]
+[QA516 Beren's Peril][QA493 Journey to Hillsbrad Foothills][QA477 Border Crossings]
+[QT477 Border Crossings][QA478 Maps and Runes]
+[QA452 Pyrewood Ambush][QT452 Pyrewood Ambush]
+[QC516 Beren's Peril]
+[QA494 Time To Strike]
+[QT493 Journey to Hillsbrad Foothills][QT494 Time To Strike]
+[A BloodElf][QT9425 Report to Tarren Mill]
+[OC][P Tarren Mill] Get the Flighpath
+[OC][F Sepulcher]
+[QT478 Maps and Runes][QT516 Beren's Peril][QA481 Dalar's Analysis][QT481 Dalar's Analysis][QA482 Dalaran's Intentions][QT482 Dalaran's Intentions][QS479 Ambermill Investigations]
+[OC][H]Hearth back to Shattrath. Take the Portal to Orgrimmar
+[QA1061 The Spirits of Stonetalon]
+[OC][P CrossRoads]
+[QA1069 Deepmoss Spider Eggs]
+[OC][P]Get the Ratchet Flightpath
+[QT1061 The Spirits of Stonetalon][QA6548 Avenge My Village][QA1062 Goblin Invaders]
+[QC6548 Avenge My Village]
+[QT6548 Avenge My Village][QA6629 Kill Grundig Darkcloud]
+[QC6629 Kill Grundig Darkcloud][QA6523 Protect Kaya]
+[QT6629 Kill Grundig Darkcloud][QT6523 Protect Kaya][QA6401 Kaya's Alive]
+[QA6461 Blood Feeders][QA6542 Report to Kadrak]
+[OC][V]Sell while here
+[QA6284 Arachnophobia]
+[QC6284 Arachnophobia][QC6461 Blood Feeders][QC1069 Deepmoss Spider Eggs]
+
+[QT6401 Kaya's Alive][QA6301 Cycle of Rebirth]
+[QA1087 Cenarius' Legacy]
+[OC][P]Get the FP at Stonetalon
+[QT6284 Arachnophobia]
+[QA6421 Boulderslide Ravine]
+[QC6421 Boulderslide Ravine]
+[QT6461 Blood Feeders][QA1058 Jin'Zil's Forest Magic]
+[QA1093 Super Reaper 6000]
+[QC1093 Super Reaper 6000][QC1062 Goblin Invaders]
+[QT1093 Super Reaper 6000][QA1094 Further Instructions]
+[QC6301 Cycle of Rebirth][QC1058 Jin'Zil's Forest Magic][QC1087 Cenarius' Legacy]
+[QT6301 Cycle of Rebirth][QT1087 Cenarius' Legacy][QA1088 Ordanus][QT6421 Boulderslide Ravine]
+[QT1058 Jin'Zil's Forest Magic]
+[QT1062 Goblin Invaders]
+[QA868 Egg Hunt][QA899 Consumed by Hatred][QA4921 Lost in Battle]
+[OC][F Ratchet]
+[QT1094 Further Instructions][QS1095 Further Instructions][QT1069 Deepmoss Spider Eggs][QA865 Raptor Horns][QA891 The Guns of Northwatch]
+[OC][R][V]Sell and repair
+[QC891 The Guns of Northwatch][QC865 Raptor Horns][QC4921 Lost in Battle]Head south on the way to Camp Taurajo
+[QA893 Weapons of Choice][QA878 Tribes at War]
+[OC][P]Camp Taurajo
+[QC899 Consumed by Hatred][QC878 Tribes at War]
+[O][QA883 Lakota'mani]Lakota is a grey kodo
+[QT883 Lakota'mani][QT878 Tribes at War]
+[QA5052 Blood Shards of Agamaggan][QT5052 Blood Shards of Agamaggan][QA879 Betrayal from Within]
+[OC][F Ratchet]
+[QT865 Raptor Horns][QT891 The Guns of Northwatch]
+[OC][F Crossroads]
+[QT899 Consumed by Hatred][QT4921 Lost in Battle]
+[OC][O]Now is a good time to Hearth and portal to Orgrimmar or Thunderbluff to train
+[OC][F Camp Taurajo]
+[OC][QT889 Spirit of the Wind]
+[QC868 Egg Hunt]
+[O][QA897 The Harvester]Is a
+[QA843 Gann's Reclamation]
+[QC879 Betrayal from Within][QC893 Weapons of Choice]
+[O][QA885 Washte Pawne] Washte Pawne is a Red Thunderhawk
+[QC843 Gann's Reclamation]
+[QT843 Gann's Reclamation][QA846 Revenge of Gann]
+[QC846 Revenge of Gann]
+[QT846 Revenge of Gann][QA849 Revenge of Gann]
+[QC849 Revenge of Gann]
+[QT849 Revenge of Gann]
+[QT879 Betrayal from Within][QT893 Weapons of Choice][QA1153 A New Ore Sample][QA906 Betrayal from Within]
+[O][QT897 The Harvester][QT885 Washte Pawne]
+[OC][F Crossroads]
+[QT868 Egg Hunt][QT906 Betrayal from Within]
+[OC][R][V] Sell, Repair, stock up on food and water
+[QT6542 Report to Kadrak][QA6543 The Warsong Reports]
+[QA6544 Torek's Assault]
+[QC6544 Torek's Assault]
+[QC6543,1 The Warsong Reports]
+[A BloodElf][QT9428 Report to Splintertree Post]
+[QT6544 Torek's Assault][QT235 The Ashenvale Hunt][QT6382 The Ashenvale Hunt]
+[OC][P]Splintertree Post
+[QC6543,3 The Warsong Reports]
+[QC6543,2 The Warsong Reports]
+[OC][P]Zoram'gar Outpost
+[OC][F Splintertree]
+[QT6543 The Warsong Reports]
+[OC][H]Hearth back to Shattrath. Take the Portal to Undercity
+[OC][F Tarren Mill]
+[QA496 Elixir of Suffering][QA498 The Rescue][QA549 WANTED: Syndicate Personnel][QA527 Battle of Hillsbrad][QA501 Elixir of Pain][QA567 Dangerous!][QA509 Elixir of Agony]
+[QC509 Elixir of Agony]
+[QC498 The Rescue][QC549 WANTED: Syndicate Personnel]
+[QT509 Elixir of Agony][QT498 The Rescue][QT549 WANTED: Syndicate Personnel][QA513 Elixir of Agony]
+[QC527 Battle of Hillsbrad][QC496 Elixir of Suffering][QC501 Elixir of Pain]
+[QT527 Battle of Hillsbrad][QT496 Elixir of Suffering][QT501 Elixir of Pain][QA499 Elixir of Suffering][QT499 Elixir of Suffering][QA502 Elixir of Pain][QA513 Elixir of Agony]
+[QT502 Elixir of Pain][QC528 Battle of Hillsbrad][QC567,2 Dangerous!][QC567,4 Dangerous!]
+[QT528 Battle of Hillsbrad][QA529 Battle of Hillsbrad]
+[QC529 Battle of Hillsbrad]
+[QT529 Battle of Hillsbrad][QA532 Battle of Hillsbrad]
+[QC532 Battle of Hillsbrad][QC567,1 Dangerous!]
+[QT532 Battle of Hillsbrad][QA539 Battle of Hillsbrad]
+[QC539 Battle of Hillsbrad][QC567 Dangerous!][QC546 Souvenirs of Death]
+[QT539 Battle of Hillsbrad][QT567 Dangerous!][QT546 Souvenirs of Death]
+[OC][F Undercity]
+[QT513 Elixir of Agony][QA515 Elixir of Agony]
+[OC][H] Hearth and take a portal to Thunderbluff or Org
+[OC][R][V][T] Train, sell, repair
+[OC][F Splintertree]
+[QA6503 Ashenvale Outrunners][QA6441 Satyr Horns][QA6571 Warsong Supplies][QA9534 Destroy the Legion]
+[QC6503 Ashenvale Outrunners][QA2 Sharptalon's Claw]Sharptalon is blue hippogryph
+[QC9534 Destroy the Legion][QA9535 Diabolical Plans]
+[QC6441 Satyr Horns][QC6571,3 Warsong Supplies]
+[QT2 Sharptalon's Claw][QT9535 Diabolical Plans][QT9534 Destroy the Legion][QT6441 Satyr Horns][QT6503 Ashenvale Outrunners][QS9536 Never Again!]
+[OC][F Zoram]
+[QA6641 Vorsha the Lasher][QA216 Between a Rock and a Thistlefur][QA6462 Troll Charm]
+[QC6641 Vorsha the Lasher]
+[QT6641 Vorsha the Lasher]
+[QC6462 Troll Charm]
+[O]If you need some extra XP then do [QA6482 Freedom to Ruul]
+[QC216 Between a Rock and a Thistlefur][QC6571,4 Warsong Supplies]
+[QA23 Ursangous's Paw]
+[QA1918 The Befouled Element] from Tideress [QC25 Stonetalon Standstill]
+[QA24 Shadumbra's Head]
+[QC1088 Ordanus]
+[QT1918 The Befouled Element][QT24][QT23][QA247 The Hunt Completed][QT247 The Hunt Completed][QT25 Stonetalon Standstill][QA824]
+[OC][F Zoram]
+[QT216 Between a Rock and a Thistlefur][QT6462 Troll Charm][QT824][QS6621 King of the Foulweald]
+[OC][F Ratchet]
+[OC][G63.65,38.6The Barrens] Take the boat to Booty Bay
+[OC][P]Get the flight point in Booty Bay
+[OC][O]Mail any extra Shreeder Pages to your bank alt
+[QC6571,2 Warsong Supplies]
+[OC]Travel back to Ratchet
+[OC][F Splintertree]
+[OC]Collect [CI4369,1] from the mailbox. Switch to an alt and buy / craft one from the AH
+[OC][QT6581 Warsong Saw Blades]
+[QC6571 Warsong Supplies]
+[OC]Collect the shredder pages from your mailbox. Switch to an alt and buy the missing pages. [CI16645,1][CI16646,1][CI16647,1][CI16648,1][CI16649,1][CI16650,1][CI16651,1][CI16652,1][CI16653,1][CI16654,1][CI16655,1][CI16656,1]
+[QT6571 Warsong Supplies][QA6504 The Lost Pages]
+[QC6504 The Lost Pages]
+[QT6504 The Lost Pages]
+[OC][F Stonetalon]
+[QT1088 Ordanus]
+[OC][F Camp Taurajo]
+[QT5881 Calling in the Reserves][QA4542 Message to Freewind Post]
+[QT4542 Message to Freewind Post][QA4841 Pacify the Centaur][QA9431 A Different Approach]
+[OC][P Freewind Post]
+[QC4821 Alien Egg] Kill all Kobalds looking for the New Ore Sample
+[QA1149 Test of Faith]
+[QC1149 Test of Faith]
+[QT1149 Test of Faith]
+[QC9431 A Different Approach][QC1153 A New Ore Sample]
+[QC4841 Pacify the Centaur]
+[QT4841 Pacify the Centaur][QT4821 Alien Egg][QA4767 Wind Rider]
+[QT4865 Serpent Wild][QT9431 A Different Approach][QA9433 A Dip in the Moonwell][QA5151 Hypercapacitor Gizmo]
+[OC][G11.98,18.82Thousand Needles]
+[QC9433 A Dip in the Moonwell]
+[QA4770 Homeward Bound][QC4770 Homeward Bound]
+[QC4767 Wind Rider]
+[QT9433 A Dip in the Moonwell][QT4770 Homeward Bound][QA9434 Testing the Tonic]
+[XP29-4050 Grind on Wyverns until you are ]
+[QT9434 Testing the Tonic][QT4767 Wind Rider]
+[OC][H]Hearth, take a portal to your races captial city.
+[OC][V][R] Vendor, Repair, stock up on new food and water.
+[OC][V]Buy your mount!
+[OC][O]Congrats on 30!
+
+]], "Summersisle's Horde Leveling guide")
